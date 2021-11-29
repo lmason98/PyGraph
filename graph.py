ac1569aa94a10374a750f150b44dfd9cd5310cc2
lmason98 <lmason98@gmail.com>
2021-11-28 18:46:27 -0800

    add graph file which will hold graph data structure

--- graph.py
+++ graph.py
@@ -2,5 +2,10 @@
 File: graph.py
 Author: Luke Mason
 
-Description:
+Description: Holds information about hte displayed graph for our various implement graphing algorithms
 """
+
+class Graph:
+
+	def __init__(self):
+		pass
